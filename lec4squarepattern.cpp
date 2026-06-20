44b6125230e169575cda53e7122a5f27d1c111ea
Rahul Kumar <devop.rahul@gmail.com>
2026-06-20 22:57:42 +0530

    Day-4 nested no. pattern

--- lec4squarepattern.cpp
+++ lec4squarepattern.cpp
@@ -10,7 +10,7 @@ int main (){
     for (int i=0 ; i<n  ; i++){
 
         char ch='A';
-        for (int j=0 ; j<n ; j++ ){
+        for (int j=0 ; j<n ;j++){
             cout << ch;
             ch++;
         }cout << endl;
