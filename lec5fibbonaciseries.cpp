5c44290b030765741a6df996810341e4533ba770
Rahul Kumar <devop.rahul@gmail.com>
2026-06-26 22:51:43 +0530

    Day-9 prime sum m0dify

---
+++ lec5fibbonaciseries.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int series(int n){
+
+    
+    
+}
+
+int main(){
+
+
+    series(7);
+    return 0;
+}
--- lec5primesum.cpp
+++ lec5primesum.cpp
@@ -1,25 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int sumprime(int n){
-
-    int sum=0;
-    for (int i=2 ; i<=n ;i++ ){
-        
-        bool isprime=true;
-
-        for (int j=2 ; j<=i-1 ; j++){
-            if (i%j==0){
-                isprime =false;
-                break;
-            }
-        }
-        if (isprime==true){ 
-            sum+=i;  
-        }
-
-    }    return sum;
+bool isprime(int n){
+    if (n<=1) return false;
+    for (int j=2; j*j<=n ; j++){
+        if(n%j==0) return false;
+    }
+    return true;
 }
+
+
 
 int main(){
 
@@ -27,7 +17,38 @@ int main(){
     cout << "enter :";
     cin >> n;
 
-    cout << sumprime(n);
+    int sum=0;
+    for (int i=2 ; i<=n ; i++){
+        if (isprime(i)){
+            sum+=i;
+        }
 
+    }cout << "sum :" << sum << endl;
+
+    bool y=isprime(n);
+    if (y==1){
+        cout << "prime";
+    }else cout <<"non prime";
+    
     return 0;
 }
+
+// int sumprime(int n){
+
+    // int sum=0;
+    // for (int i=2 ; i<=n ;i++ ){
+        
+        // bool isprime=true;
+
+        // for (int j=2 ; j<=i ; j++){
+            // if (i%j==0){
+                // isprime =false;
+                // break;
+            // }
+        // }
+        // if (isprime==true){ 
+            // sum+=i;  
+        // }
+
+    // }    return sum;
+// }
