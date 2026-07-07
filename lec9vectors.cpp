33615e2909f5dc114b459e8f15eaa3b744c840dd
Rahul Kumar <devop.rahul@gmail.com>
2026-07-07 22:11:16 +0530

    Day-18 vectors

---
+++ lec9vectors.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main (){
+
+    vector<int> vec;
+    this is vector with no sizze or no stored value
+    cout << vec[0]; this file will  not be accessible >> segmentation error
+
+     vector <int> vec={1,2,3}; automatic size becomes 3 becoz vector works diff. from array 
+     becoz array is fixed and vector can expand;
+     cout << vec[0];
+     this prints value in vector
+     
+    vector<int> vec(3,0); // 3 is size and 0 is value assigned to all index; _0_0_0.
+    cout << vec[0] << " ";
+    cout << vec[1] << " ";
+    cout << vec[2] << " " << endl;
+
+    for(int i : vec){
+        cout <<i <<endl; //here the index i is stored value on the index or say for(int val : vec)
+    }
+
+    vector<int> vec;
+
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+    vec.push_back(40);
+
+    cout << "intial size = " << vec.size() << endl ;
+
+    vec.pop_back();
+    cout << "final size = " << vec.size() << endl;
+
+    cout << vec.front() << ' ';
+    cout << vec.at(1) << " ";
+    cout << vec.back();
+    for (int value : vec)
+        cout << value << endl;
+    }
+
+    return 0; 
+}
